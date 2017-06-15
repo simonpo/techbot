f8c0afc333256ec96ba0e681f5a941f48c38acc4
Simon Powell <simonpo@microsoft.com>
2017-06-14 18:05:29 -0700

    Added Azure search

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ appInsights.start();
 // and other requirements
 var restify = require('restify'); 
 var builder = require('botbuilder'); 
+require('./searchHelpers.js')();
 
 // Setup Restify Server
 var server = restify.createServer();
@@ -30,9 +31,51 @@ var intents = new builder.IntentDialog({ recognizers: [recognizer] });
 // Create bot dialogs
 bot.dialog('/', intents);
 intents.matches('Greeting', builder.DialogAction.send('Hello'));
+intents.matches('Search', [
+    function(session, results) {
+    var name = results.entities[0]["entity"];
+    var queryString = 'https://' + process.env.AZURE_SEARCH_NAME + '.search.windows.net/indexes/' + process.env.AZURE_INDEX_NAME + '/docs?api-key=' + process.env.AZURE_SEARCH_KEY + '&api-version=2015-02-28&' + 'search=' + name;
+
+    performSearchQuery(queryString, function (err, result) {
+        if (err) {
+            console.log("Error when searching for article: " + err);
+        } else if (result && result['value'] && result['value'][0]) {
+            //If we have results send them to the showResults dialog (acts like a decoupled view)
+            session.replaceDialog('/showResults', { result });
+        } else {
+            session.endDialog("No musicians by the name \'" + name + "\' found");
+        }
+    })
+
+    //console.log(queryString);
+    //if(results.entities[0]) {
+    //    session.send(results.entities[0]["entity"])
+    //    session.send(results.entities[0]["type"])
+    //    }
+    }
+]);
 intents.matches('Help', builder.DialogAction.send("Basic help information goes here."));
 intents.matches('AboutTheBot', builder.DialogAction.send("I'm a chat bot, built using the botscaffold Yeoman generator."));
 intents.onDefault(builder.DialogAction.send("Sorry, but I didn't understand that. Type Help to get some help."));
+
+// show results
+
+bot.dialog('/showResults', [
+    function (session, args) {
+        var msg = new builder.Message(session).attachmentLayout(builder.AttachmentLayout.carousel);
+            args.result['value'].forEach(function (article, i) {
+                msg.addAttachment(
+                    new builder.HeroCard(session)
+                        .title(article.Title)
+                        .subtitle(article.Subtitle + " | " + "Search Score: " + article['@search.score'])
+                        .text(article.Body[0])
+                        // .images([builder.CardImage.create(session, article.imageURL)])
+                );
+            })
+        session.endDialog(msg);
+    }
+])
+
 
 // web interface
 server.get('/', restify.serveStatic({

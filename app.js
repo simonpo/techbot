fbfe63a1397a2b4ab4bdfb6fcf678cfefe1cbb1e
Simon Powell <simonpo@microsoft.com>
2017-06-16 18:45:46 -0700

    Slack specific formatting tests

--- app.js
+++ app.js
@@ -60,10 +60,6 @@ intents.matches('Search', [
     })
 
     console.log(queryString);
-    //if(results.entities[0]) {
-    //    session.send(results.entities[0]["entity"])
-    //    session.send(results.entities[0]["type"])
-    //    }
     }
 ]);
 intents.matches('Help', builder.DialogAction.send("Basic help information goes here."));
@@ -113,7 +109,7 @@ intents.onDefault(builder.DialogAction.send("Sorry, but I didn't understand that
 bot.dialog('/showResults', [
     function (session, args) {
         args.result['value'].forEach(function (article, i) {
-            var msg = ("Title: " + (article.Title) + "\nBody: " + (article.Body.join('\n') + "\n\n Relevance: " + (article['@search.score'])));
+            var msg = ("*" + (article.Title) + "*\n\n" + (article.Body.join('\n') + "\n\n `Relevance: " + (article['@search.score']) +"`"));
             session.endDialog(msg);
             //session.send("Title: " + (article.Title) + "\nBody: " + (article.Body.join('\n') + "\n\n Relevance: " + (article['@search.score'])));
         })
